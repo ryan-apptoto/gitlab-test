cc03fffd1e508dc99f3f26060c45187d4b238b7f
Ryan McAdler <ryan@apptoto.com>
2018-08-06 14:50:04 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GitlabTest::Application.config.secret_token = 'fb181733c43133687550e39eb19a65c6fe3c5cc513b457e2bb5339ca7eba23fb97df433986cb16b60b1093b505be7404f8f407d3e2c1f042a24d7ed39bbddedc'
